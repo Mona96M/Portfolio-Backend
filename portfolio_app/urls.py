27617067c6fc1a9ce7f78f192473705b03591500
Mona96M <monaalshahrani96@gmail.com>
2025-04-30 12:01:53 +0300

    Add URL for SkillListCreateView

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import EducationListCreateView
+from .views import EducationListCreateView, SkillListCreateView
 
 urlpatterns = [
-    path('educations/', EducationListCreateView.as_view(), name='education-list-create')
+    path('educations/', EducationListCreateView.as_view(), name='education-list-create'),
+    path('skills/', SkillListCreateView.as_view(), name='skill-list-create'),
 ]
