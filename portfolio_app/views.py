e8f65fd31bc646942d4fa07b8d9f81fd234ac122
Mona96M <monaalshahrani96@gmail.com>
2025-04-30 09:13:00 +0300

    Add Post method to EducationListCreateView

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -11,3 +11,10 @@ class EducationListCreateView(APIView):
         education = Education.objects.all()
         serializer = EducationSerializer(education, many=True)
         return Response(serializer.data, status=200)
+    
+    def post(self, request):
+        serializer = EducationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
