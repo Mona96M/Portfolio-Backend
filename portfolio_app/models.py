67badd4d6ccc9ee13d51f2c77fef7b34fed9cbd2
Mona96M <monaalshahrani96@gmail.com>
2025-04-30 08:19:51 +0300

    Add Education Model

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Education(models.Model):
+    university = models.CharField(max_length=50)
+    major = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.university
