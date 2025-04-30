dd5ea0c47cf4127d035dbb51aed492c2a9e2a113
Mona96M <monaalshahrani96@gmail.com>
2025-04-30 15:45:58 +0300

    Add URL for ProjectListCreateView and ProjectDetailView

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import EducationListCreateView, SkillListCreateView, SkillDetailView
+from .views import ( EducationListCreateView,
+                    SkillListCreateView, 
+                    SkillDetailView,
+                    ProjectListCreateView,
+                    ProjectDetailView,
+                    )
 
 urlpatterns = [
     path('educations/', EducationListCreateView.as_view(), name='education-list-create'),
     path('skills/', SkillListCreateView.as_view(), name='skill-list-create'),
     path('skills/<int:pk>/', SkillDetailView.as_view(), name='skill-detail'),
+    path('projects/', ProjectListCreateView.as_view(), name='project-list-create'),
+    path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
+
 ]
