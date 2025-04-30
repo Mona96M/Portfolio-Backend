d609fd4e9d884e9c383527249bb8e1e00dacb9d1
Mona96M <monaalshahrani96@gmail.com>
2025-04-30 12:04:26 +0300

    Add URL for SkillDetailView

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import EducationListCreateView, SkillListCreateView
+from .views import EducationListCreateView, SkillListCreateView, SkillDetailView
 
 urlpatterns = [
     path('educations/', EducationListCreateView.as_view(), name='education-list-create'),
     path('skills/', SkillListCreateView.as_view(), name='skill-list-create'),
+    path('skills/<int:pk>/', SkillDetailView.as_view(), name='skill-detail'),
 ]
