34dab593f47de65c9d019ad6173bd449ddb19367
Mona96M <monaalshahrani96@gmail.com>
2025-04-30 08:45:26 +0300

    Create EducationListCreateView to list all Education entries

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+from .models import Education
+from .serializers import EducationSerializer
 # Create your views here.
+
+class EducationListCreateView(APIView):
+    def get(self, request):
+        education = Education.objects.all()
+        serializer = EducationSerializer(education, many=True)
+        return Response(serializer.data, status=200)
