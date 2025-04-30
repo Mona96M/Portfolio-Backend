1258950e5247708a9fc31b7ad700083d9d71f327
Mona96M <monaalshahrani96@gmail.com>
2025-04-30 09:57:54 +0300

    Add Skill Serializer

--- portfolio_app/serializers.py
+++ portfolio_app/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Education
+from .models import Education, Skill
 
 class EducationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Education
+        fields = '__all__'
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
         fields = '__all__'
